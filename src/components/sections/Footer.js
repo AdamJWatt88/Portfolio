f5496ba55902fda0020b8d57f7e8799b31590f41
Adam <adamjwatt88@gmail.com>
2022-04-09 17:55:05 -0700

    ready for deployment

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -1,63 +1,127 @@
-import { graphql, useStaticQuery } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-import React from "react"
+//* original import
+// import { graphql, useStaticQuery } from "gatsby"
+import { graphql, StaticQuery } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+import React from "react";
+
+//* original component
+// const Footer = () => {
+//   const query = useStaticQuery(
+//     graphql`
+//       query Footer {
+//         markdownRemark(fileAbsolutePath: { regex: "/footer.md/" }) {
+//           frontmatter {
+//             title
+//             email
+//             link
+//             link2
+//             frontend_link
+//             github_link
+//           }
+//         }
+//       }
+//     `
+//   )
+
+//   const data = query
+//   const { email, link, link2, title, frontend_link, github_link } =
+//     data.markdownRemark.frontmatter
+
+//   return (
+//     <footer id="footer" className="footer">
+//       <div className="footer__content">
+//         <h2>{title}</h2>
+//         <div className="footer__email">
+//           <StaticImage
+//             src="../../images/icons/mail.svg"
+//             alt="email"
+//             layout="fixed"
+//             width={50}
+//           />
+//           <p>{email}</p>
+//         </div>
+//         <a href={frontend_link} className="footer__link footer__link-frontend">
+//           <StaticImage
+//             src="../../images/icons/frontend-mentor.svg"
+//             alt="Frontend Mentor"
+//             layout="fixed"
+//             width={50}
+//           />
+//           <p>{link}</p>
+//         </a>
+//         <a href={github_link} className="footer__link footer__link-github">
+//           <StaticImage
+//             src="../../images/icons/github2.svg"
+//             alt="Github"
+//             layout="fixed"
+//             width={50}
+//           />
+//           <p>{link2}</p>
+//         </a>
+//       </div>
+//     </footer>
+//   )
+// }
+
+// export default Footer
 
 const Footer = () => {
-  const query = useStaticQuery(
-    graphql`
-      query Footer {
-        markdownRemark(fileAbsolutePath: { regex: "/footer.md/" }) {
-          frontmatter {
-            title
-            email
-            link
-            link2
-            frontend_link
-            github_link
+  return (
+    <StaticQuery
+      query={graphql`
+        query Footer {
+          markdownRemark(fileAbsolutePath: { regex: "/footer.md/" }) {
+            frontmatter {
+              title
+              email
+              link
+              link2
+              frontend_link
+              github_link
+            }
           }
         }
-      }
-    `
-  )
+      `}
+      render={(data) => (
+        <footer id='footer' className='footer'>
+          <div className='footer__content'>
+            <h2>{data.markdownRemark.frontmatter.title}</h2>
+            <div className='footer__email'>
+              <StaticImage
+                src='../../images/icons/mail.svg'
+                alt='email'
+                layout='fixed'
+                width={50}
+              />
+              <p>{data.markdownRemark.frontmatter.email}</p>
+            </div>
+            <a
+              href={data.markdownRemark.frontmatter.frontend_link}
+              className='footer__link footer__link-frontend'>
+              <StaticImage
+                src='../../images/icons/frontend-mentor.svg'
+                alt='Frontend Mentor'
+                layout='fixed'
+                width={50}
+              />
+              <p>{data.markdownRemark.frontmatter.link}</p>
+            </a>
+            <a
+              href={data.markdownRemark.frontmatter.github_link}
+              className='footer__link footer__link-github'>
+              <StaticImage
+                src='../../images/icons/github2.svg'
+                alt='Github'
+                layout='fixed'
+                width={50}
+              />
+              <p>{data.markdownRemark.frontmatter.link2}</p>
+            </a>
+          </div>
+        </footer>
+      )}
+    />
+  );
+};
 
-  const data = query
-  const { email, link, link2, title, frontend_link, github_link } =
-    data.markdownRemark.frontmatter
-
-  return (
-    <footer id="footer" className="footer">
-      <div className="footer__content">
-        <h2>{title}</h2>
-        <div className="footer__email">
-          <StaticImage
-            src="../../images/icons/mail.svg"
-            alt="email"
-            layout="fixed"
-            width={50}
-          />
-          <p>{email}</p>
-        </div>
-        <a href={frontend_link} className="footer__link footer__link-frontend">
-          <StaticImage
-            src="../../images/icons/frontend-mentor.svg"
-            alt="Frontend Mentor"
-            layout="fixed"
-            width={50}
-          />
-          <p>{link}</p>
-        </a>
-        <a href={github_link} className="footer__link footer__link-github">
-          <StaticImage
-            src="../../images/icons/github2.svg"
-            alt="Github"
-            layout="fixed"
-            width={50}
-          />
-          <p>{link2}</p>
-        </a>
-      </div>
-    </footer>
-  )
-}
-
-export default Footer
+export default Footer;
--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -1,8 +1,88 @@
 import React, { useState, useEffect } from "react";
-import { graphql, Link, useStaticQuery } from "gatsby";
+//*f original import
+// import { graphql, Link, useStaticQuery } from "gatsby";
+import { graphql, Link, StaticQuery } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
 
 import Menu from "../Menu";
+
+//* original component
+// const Hero = () => {
+//   const isBrowser = () => typeof window !== "undefined";
+
+//   const [isDesktop, setDesktop] = useState(
+//     isBrowser() && window.innerWidth > 600
+//   );
+
+//   const updateMedia = () => {
+//     setDesktop(isBrowser() && window.innerWidth > 600);
+//   };
+
+//   useEffect(() => {
+//     isBrowser() && window.addEventListener("resize", updateMedia);
+//     return () =>
+//       isBrowser() && window.removeEventListener("resize", updateMedia);
+//   });
+
+//   const closeMenu = () => {
+//     isBrowser() &&
+//       document.querySelector("body").classList.remove("modal-open");
+//   };
+
+//   const data = useStaticQuery(
+//     graphql`
+//       query SiteHeader {
+//         site {
+//           siteMetadata {
+//             author
+//             description
+//             title
+//           }
+//         }
+//       }
+//     `
+//   );
+
+//   const { author, description, title } = data.site.siteMetadata;
+
+//   const renderNav = () => {
+//     return (
+//       <div className='hero__nav'>
+//         <Link onClick={closeMenu} className='hero__nav-link' to='#about-me'>
+//           About
+//         </Link>
+//         <Link onClick={closeMenu} className='hero__nav-link' to='#projects'>
+//           Projects
+//         </Link>
+//         <Link onClick={closeMenu} className='hero__nav-link' to='#footer'>
+//           Contact
+//         </Link>
+//       </div>
+//     );
+//   };
+
+//   return (
+//     <header className='hero'>
+//       {isDesktop ? renderNav() : <Menu />}
+//       <StaticImage
+//         className='hero__avatar'
+//         src='../../images/avatar/avatar.svg'
+//         alt='avatar'
+//         placeholder='blurred'
+//         layout='constrained'
+//         width={300}
+//         height={300}
+//       />
+//       <div className='hero__content'>
+//         <h1 className='hero__header'>{title}</h1>
+//         <h2 className='hero__author'>{author}</h2>
+//         <p className='hero__description'>{description}</p>
+//       </div>
+//     </header>
+//   );
+// };
+
+// export default Hero;
 
 const Hero = () => {
   const isBrowser = () => typeof window !== "undefined";
@@ -26,22 +106,6 @@ const Hero = () => {
       document.querySelector("body").classList.remove("modal-open");
   };
 
-  const data = useStaticQuery(
-    graphql`
-      query SiteHeader {
-        site {
-          siteMetadata {
-            author
-            description
-            title
-          }
-        }
-      }
-    `
-  );
-
-  const { author, description, title } = data.site.siteMetadata;
-
   const renderNav = () => {
     return (
       <div className='hero__nav'>
@@ -59,23 +123,40 @@ const Hero = () => {
   };
 
   return (
-    <header className='hero'>
-      {isDesktop ? renderNav() : <Menu />}
-      <StaticImage
-        className='hero__avatar'
-        src='../../images/avatar/avatar.svg'
-        alt='avatar'
-        placeholder='blurred'
-        layout='constrained'
-        width={300}
-        height={300}
-      />
-      <div className='hero__content'>
-        <h1 className='hero__header'>{title}</h1>
-        <h2 className='hero__author'>{author}</h2>
-        <p className='hero__description'>{description}</p>
-      </div>
-    </header>
+    <StaticQuery
+      query={graphql`
+        query SiteHeader {
+          site {
+            siteMetadata {
+              author
+              description
+              title
+            }
+          }
+        }
+      `}
+      render={(data) => (
+        <header className='hero'>
+          {isDesktop ? renderNav() : <Menu />}
+          <StaticImage
+            className='hero__avatar'
+            src='../../images/avatar/avatar.svg'
+            alt='avatar'
+            placeholder='blurred'
+            layout='constrained'
+            width={300}
+            height={300}
+          />
+          <div className='hero__content'>
+            <h1 className='hero__header'>{data.site.siteMetadata.title}</h1>
+            <h2 className='hero__author'>{data.site.siteMetadata.author}</h2>
+            <p className='hero__description'>
+              {data.site.siteMetadata.description}
+            </p>
+          </div>
+        </header>
+      )}
+    />
   );
 };
 
