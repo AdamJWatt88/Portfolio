22da3e6a929d1cba9ba7c6c66f9c99cd3ef066a6
Adam <adamjwatt88@gmail.com>
2022-07-31 13:41:08 -0700

    added loopstudios

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-remark-images`,
     `gatsby-plugin-sass`,
+    `gatsby-plugin-remove-serviceworker`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
